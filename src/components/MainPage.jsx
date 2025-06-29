aff7759c6309a3a5f2f32a54c73929b82e224569
88TOUR <pcybywater1125@gmail.com>
2025-06-30 01:32:13 +0900

    시작

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -11,7 +11,7 @@ const MainPage = ({ onStart }) => (
       <img src="/cloud2.png" alt="구름2" className="cloud-img" />
     </div>
     <div className="cloud cloud4">
-      <img src="/cloud4.png" alt="구름4" className="cloud-img" />
+      <img src="/cloud1.png" alt="구름4" className="cloud-img" />
     </div>
 
     {/* 중앙 컨텐츠 */}
