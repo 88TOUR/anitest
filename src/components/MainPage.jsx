bace1f97dd807d75be829e02b731f6045e567503
88TOUR <pcybywater1125@gmail.com>
2025-07-01 14:54:04 +0900

    스타일

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -21,7 +21,7 @@ const MainPage = ({ onStart }) => (
         alt="여행 로고"
         className="main-logo"
       />
-      <h1 className="main-title">나만의 여행 성향 테스트</h1>
+      <h1 className="main-title">나의 여행 성향 테스트</h1>
       <p className="main-description">
         나의 여행 유형을 통해<br />
         나만의 여행을 디자인하라!
